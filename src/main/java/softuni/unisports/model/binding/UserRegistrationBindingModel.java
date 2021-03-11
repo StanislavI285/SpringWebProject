1e829bce4e7e2081f9a5f9f109c2e8ae8168536f
Stanislav Ivanov <iivanov.stanislav@gmail.com>
2021-03-11 22:31:45 +0200

    Resources

---
+++ src/main/java/softuni/unisports/model/binding/UserRegistrationBindingModel.java
@@ -0,0 +1,86 @@
+package softuni.unisports.model.binding;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class UserRegistrationBindingModel {
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String confirmPassword;
+
+    public UserRegistrationBindingModel() {
+    }
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    public String getUsername() {
+        return username;
+    }
+
+    public UserRegistrationBindingModel setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    @NotBlank
+    @Size(min = 2)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public UserRegistrationBindingModel setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    @NotBlank
+    @Size(min = 2)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public UserRegistrationBindingModel setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    @NotBlank
+    @Email
+    public String getEmail() {
+        return email;
+    }
+
+    public UserRegistrationBindingModel setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+
+    //Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")
+    public String getPassword() {
+        return password;
+    }
+
+    public UserRegistrationBindingModel setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    @NotBlank
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public UserRegistrationBindingModel setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+        return this;
+    }
+}
