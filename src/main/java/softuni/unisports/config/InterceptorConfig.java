a022e53ba8aec8ad6a9794828a9e28e2bf749faa
Stanislav Ivanov <iivanov.stanislav@gmail.com>
2021-03-19 09:44:31 +0200

    Processing time interceptor added

---
+++ src/main/java/softuni/unisports/config/InterceptorConfig.java
@@ -0,0 +1,21 @@
+package softuni.unisports.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import softuni.unisports.interceptors.LogInterceptor;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    private final LogInterceptor logInterceptor;
+
+    public InterceptorConfig(LogInterceptor logInterceptor) {
+        this.logInterceptor = logInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
+    }
+}
---
+++ src/main/java/softuni/unisports/interceptors/LogInterceptor.java
@@ -0,0 +1,46 @@
+package softuni.unisports.interceptors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory
+            .getLogger(LogInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) {
+
+//        long startTime = System.currentTimeMillis();
+//        logger.info("Request URL::" + request.getRequestURL().toString()
+//                + ":: Start Time=" + System.currentTimeMillis());
+        request.setAttribute("startTime", System.currentTimeMillis());
+        return true;
+    }
+
+    //@Override
+//    public void postHandle(HttpServletRequest request,
+//                           HttpServletResponse response, Object handler,
+//                           ModelAndView modelAndView) {
+//      logger.info("Request URL::" + request.getRequestURL().toString()
+//                + " Sent to Handler :: Current Time=" + System.currentTimeMillis());
+//    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response, Object handler, Exception ex) {
+        long startTime = (Long) request.getAttribute("startTime");
+        logger.info("Request URL::" + request.getRequestURL().toString()
+                + ":: End Time=" + System.currentTimeMillis());
+        logger.info("Request URL::" + request.getRequestURL().toString()
+                + ":: Time Taken=" + (System.currentTimeMillis() - startTime));
+    }
+}
